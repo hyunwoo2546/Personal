47b1186e2cd43c377660db3eff7eebd24ed52558
Hyunwoo <hyunwoo2546@naver.com>
2022-02-22 19:58:45 +0900

    2/22

---
+++ Personal_K/src/main/java/com/hyun/controller/BoardController.java
@@ -0,0 +1,14 @@
+package com.hyun.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+
+	@GetMapping("/test")
+	public void Test() {
+		
+	}
+	
+}
