45915a2a7f6b5ff5fd2ef184133c7e37b78407eb
Hyunwoo <hyunwoo2546@naver.com>
2022-02-24 00:43:03 +0900

    2/24

--- Personal_K/src/main/java/com/hyun/service/BoardService.java
+++ Personal_K/src/main/java/com/hyun/service/BoardService.java
@@ -3,7 +3,7 @@ package com.hyun.service;
 import java.util.List;
 
 import com.hyun.vo.BoardVO;
-import com.hyun.vo.Criteria;
+import com.hyun.vo.SearchCriteria;
 
 public interface BoardService {
 
@@ -11,10 +11,10 @@ public interface BoardService {
 	public void write(BoardVO vo) throws Exception;
 	
 	/* # 게시물 목록 조회 */
-	public List<BoardVO> list(Criteria cri) throws Exception;
+	public List<BoardVO> list(SearchCriteria scri) throws Exception;
 	
 	/* # 게시물 총 갯수 */
-	public int listCount() throws Exception;
+	public int listCount(SearchCriteria scri) throws Exception;
 	
 	/* # 특정 게시글 조회 */
 	public BoardVO read(int bno) throws Exception;
